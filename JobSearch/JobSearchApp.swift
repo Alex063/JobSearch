6eee6b30b243f8b98f8fafe5d5bb5434a7a6b5f3
Aleksandr Gulakov <aleksandr.gulakov@gmail.com>
2024-03-14 20:19:49 +0500

    Initial Commit

---
+++ JobSearch/JobSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobSearchApp.swift
+//  JobSearch
+//
+//  Created by Aleksandr Gulakov on 14.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct JobSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
